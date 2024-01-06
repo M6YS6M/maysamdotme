2a2cf495e5f63faa4ba649d10ac18f5b1fe7e134
May Sam <maysssam@gmail.com>
2024-01-05 18:58:34 -0800

    Initial commit

---
+++ app/projects/181114-wildWood/page.tsx
@@ -0,0 +1,51 @@
+import Image from 'next/image';
+const projects = [
+  {
+    id: 1,
+    name: 'Wildwood',
+    href: '/projects/071009-membrane#',
+    Date: 'xyz',
+    imageSrc: '/thumbnail/20_wildwood_thumb_zimarc.png',
+    imageAlt: 'tensile membrane structures architecture by zimarc ',
+  },
+
+  // More products...
+]
+
+// Create a function to import all images from a specific directory
+function importAll(r) {
+  return r.keys().map(r);
+}
+
+// Use require.context to get all image files from the specified directory
+const images = importAll(require.context('./img/', false, /\.(png|jpe?g|svg)$/));
+// Now, 'images' is an array containing the imported images
+
+
+export default function project() {
+  return (
+    <div className='h-auto max-w-full'>
+      <h1 className="mt-1 text-sm text-gray-700 uppercase">wildwood</h1>
+
+      {/* Loop through all images and render each one */}
+      {images.map((image, index) => (
+        <div key={index} className="mx-auto max-w-screen-xl" style={{ padding: '10px ', }}>
+          <Image
+            src={image.default}
+            alt={`Image ${index + 1}`}
+            className="mx-auto max-w-80vw sm:max-w-60vw md:max-w-40vw lg:max-w-30vw xl:max-w-25vw"
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+
+
+//title: "Membrane"
+//cover: "./thumbnail/thumb-zimarc.png"
+//date: "2007-10-09"
+//category: "Architecture"
+//size: "l"
+//tag: "good,nice"
